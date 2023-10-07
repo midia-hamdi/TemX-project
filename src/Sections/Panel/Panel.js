7309a729bd069ea510b233be5cd6ad2afd5ace23
midia-hamdi <midiahamdi367@gmail.com>
2023-10-07 17:29:20 +0330

    Add image user, text, and item

--- src/Sections/Panel/Panel.js
+++ src/Sections/Panel/Panel.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import Logo from '../../Sections/Panel/Logo'
-import ImageUser from '../../Sections/Panel/ImageUser'
-import TextUser from './TextUser'
+import React from 'react';
+import Logo from '../../Sections/Panel/Logo';
+import ImageUser from '../../Sections/Panel/ImageUser';
+import TextUser from './TextUser';
+import Rectangle from './Rectangle';
 
 export default function Panel() {
   return (
     <div className='Pannel-section'>
-        <Logo />
+      <Logo />
+      <div className="user-container">
         <ImageUser />
-        <TextUser />
+        <div className="rectangles">
+          <Rectangle color='var(--non-selected-blue-all, #DFEEFF)' />
+          <Rectangle color='var(--non-selected-blue-all, #DFEEFF)' />
+          <Rectangle color='var(--gradient-1, linear-gradient(68deg, #0066E2 32.63%, #1D82FF 70.87%))' />
+        </div>
+      </div>
+      <TextUser />
     </div>
-  )
+  );
 }
---
+++ src/Sections/Panel/Rectangle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Rectangle({color}) {
+  return (
+    
+        <div className='rectangle' style={{ background: color }}></div>
+  )
+}
