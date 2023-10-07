54f52f13f4e5f62c922d6f7f9430980ca8eafdb7
midia-hamdi <midiahamdi367@gmail.com>
2023-10-07 18:12:15 +0330

    Add image user, text, and item

--- src/Pages/Authentication/Authentication.js
+++ src/Pages/Authentication/Authentication.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Panel from '../../Sections/Panel/Panel.js'
+import NavBar from '../../Sections/NavBar/NavBar.js'
+
 
 export default function Authentication() {
   return (
     <div>
         <Panel />
+        <NavBar />
     </div>
   )
 }
