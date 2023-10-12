5b186c2db23d1928b853dd9f1a931a34ede76f0f
midia-hamdi <midiahamdi367@gmail.com>
2023-10-12 23:44:43 +0330

    Add nex section and style

--- src/Sections/Authentication/Authentication.js
+++ src/Sections/Authentication/Authentication.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import TextAuthentication from './TextAuthentication';
 import Rectangle from '../../Components/Rectangle';
-
-const coloredRectangleStyle = {
-  background: 'var(--non-selected-blue-all, #DFEEFF)',
-  width: '0.8375rem',
-  height: '2.5rem',
-  flexShrink: 0,
-  // ... other custom styles
-};
-
-const gradientRectangleStyle = {
-  background: 'var(--gradient-1, linear-gradient(68deg, #0066E2 32.63%, #1D82FF 70.87%))',
-  width: '0.8375rem',
-  height: '2.5rem',
-  // ... other custom styles
-};
+import { coloredRectangleStyle, gradientRectangleStyle } from '../../Styles/Authenticationcss';
 
 export default function Authentication() {
   return (
     <div>
       <TextAuthentication />
-      <div className="rectangle-container">
         <div className="rectangle-row">
-          <Rectangle style={coloredRectangleStyle} />
-          <Rectangle style={coloredRectangleStyle} />
-          <Rectangle style={gradientRectangleStyle} />
+          <Rectangle style={coloredRectangleStyle} className="rectanglespace" />
+          <Rectangle style={coloredRectangleStyle} className="rectanglespace" />
+          <Rectangle style={gradientRectangleStyle} className="rectanglespace" />
         </div>
       </div>
-    </div>
   );
 }
+
+
---
+++ src/Styles/Authenticationcss.js
@@ -0,0 +1,14 @@
+export const coloredRectangleStyle = {
+    background: 'var(--non-selected-blue-all, #DFEEFF)',
+    width: '0.8375rem',
+    height: '2.5rem',
+    flexShrink: 0,
+    // ... other custom styles
+  };
+  
+  export const gradientRectangleStyle = {
+    background: 'var(--gradient-1, linear-gradient(68deg, #0066E2 32.63%, #1D82FF 70.87%))',
+    width: '0.8375rem',
+    height: '2.5rem',
+    // ... other custom styles
+  };
