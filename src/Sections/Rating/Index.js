4f1ffdd0181d59596774cddcedf02c31957f240c
midia-hamdi <midiahamdi367@gmail.com>
2023-11-07 16:10:08 +0330

    Start respansive web

--- src/Sections/Rating/Index.js
+++ src/Sections/Rating/Index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import TextAuthentication from './Text.js';
 import Rectangle from '../../Components/Rectangle.js';
 import { coloredRectangleStyle, gradientRectangleStyle } from '../../Styles/Authenticationcss.js';
-import { SizeRectangleStyle } from '../../Styles/SizeRectangle.js';
+// import { SizeRectangleStyle } from '../../Styles/SizeRectangle.js';
 import Level from './Level.js';
-import ImgAuthentication from './Img.js';
+import Img from './Img.js';
 import TextLevel from './TextLevel.js';
 import '../../Sections/Rating/Style/Style.css'
 
@@ -12,16 +12,16 @@ export default function Authentication() {
   return (
     <div>
         <TextAuthentication />
-                <div className='rectangle-auth'>
+                <div className='rectangle-auth'></div>
                     <Rectangle style={gradientRectangleStyle} className='small-rectangle1'/>
-                    <Rectangle style={SizeRectangleStyle} className="large-rectangle1"/>
+                    <Rectangle className="SizeRectangleStyle"/>
                     <Rectangle style={coloredRectangleStyle} className="small-rectangle2"/>
-                    <Rectangle style={SizeRectangleStyle} className="large-rectangle2"/>
+                    <Rectangle className="SizeRectangleStyle1"/>
                     <Rectangle style={coloredRectangleStyle} className="small-rectangle3"/>
-                </div>
+                
             
             <Level texts={["سطح برنزی", "سطح نقره‌ای", "سطح طلایی"]}  />
-            <ImgAuthentication />
+            <Img />
             <TextLevel />
         </div>
 
--- src/Styles/SizeRectangle.js
+++ src/Styles/SizeRectangle.js
@@ -1,9 +1,10 @@
-export const SizeRectangleStyle = {
-    background: 'var(--non-selected-blue-all, #DFEEFF)',
-    width: '11.3125rem',
-    height: '0.1875rem',
-    top: 0,
-    margin: '2rem 0.4rem 0 0',
+// export const SizeRectangleStyle = {
+//     background: 'var(--non-selected-blue-all, #DFEEFF)',
+//     width: '11.3125rem',
+//     height: '0.1875rem',
+//     top: 0,
+//     margin: '2rem 0.4rem 0 0',
 
-    // ... other custom styles
-  };
+//     // ... other custom styles
+//   };
+  
